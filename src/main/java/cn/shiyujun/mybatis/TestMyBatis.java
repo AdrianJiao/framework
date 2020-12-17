ae5fa889ef4fea43b4affc1122b4890f96e37f88
jiaoshaofei <jiaoshaofei@jmfen.cn>
2020-12-17 11:47:41 +0800

    提交

--- src/main/java/cn/shiyujun/mybatis/TestMyBatis.java
+++ src/main/java/cn/shiyujun/mybatis/TestMyBatis.java
@@ -21,7 +21,11 @@ public class TestMyBatis {
         try {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             User user = new User(1, "lisi", new Integer(25));
+
             userMapper.insertUser(user);
+
+            User user2 = new User(2, "zhangsan", new Integer(25));
+            userMapper.insertUser(user2);
             sqlSession.commit();// 这里一定要提交，不然数据进不去数据库中
         } finally {
             sqlSession.close();
