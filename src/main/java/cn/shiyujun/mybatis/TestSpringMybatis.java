77d20f36e9651971922a29f0f88dd22e5f2027b0
jiaoshaofei <jiaoshaofei@jmfen.cn>
2020-12-26 16:44:27 +0800

    1.修改依赖版本

--- src/main/java/cn/shiyujun/mybatis/TestSpringMybatis.java
+++ src/main/java/cn/shiyujun/mybatis/TestSpringMybatis.java
@@ -12,11 +12,22 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpringMybatis {
 
     public static void main(String[] args) {
+        testMapper();
+        testMapperFactoryBeanMapper();
+    }
+
+    public static void testMapper(){
         ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-mybatis.xml");
         //<mybatis:scan> 注入的mapper
         UserMapper userMapper = context.getBean("userMapper", UserMapper.class);
-        //MapperFactoryBean 注入的mapper
-//        UserMapper userMapper = context.getBean("userMapper2", UserMapper.class);
+        User user = userMapper.getUser("zhangsan");
+        System.out.println("name: " + user.getName() + "|age: " + user.getAge());
+    }
+
+    public static void testMapperFactoryBeanMapper(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:application-mybatis.xml");
+        //MapperFactoryBean 注入的mapper, 获取工厂本事用name &userMapper2
+        UserMapper userMapper = context.getBean("userMapper2", UserMapper.class);
         User user = userMapper.getUser("zhangsan");
         System.out.println("name: " + user.getName() + "|age: " + user.getAge());
     }
